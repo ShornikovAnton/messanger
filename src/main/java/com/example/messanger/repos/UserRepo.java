c9f7550d64331464b9e7d37c0d3fb69096fa0a55
Sh_Anton <Shornikov.anthon@yandex.ru>
2022-04-08 03:16:16 +0500

    Закоммитил проект

---
+++ src/main/java/com/example/messanger/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package com.example.messanger.repos;
+
+import com.example.messanger.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
